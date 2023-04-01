904168ce0b8944eec02fd8a1066b665870f5cde5
ghazi <ghaziabbassi1@gmail.com>
2023-04-01 15:19:05 +0100

    Bump to 0.8.0

--- flake8_stash/__init__.py
+++ flake8_stash/__init__.py
@@ -1,3 +1,3 @@
 """A collection of flake8 checks."""
 
-__version__ = "0.7.0"
+__version__ = "0.8.0"
--- flake8_stash/plugin.py
+++ flake8_stash/plugin.py
@@ -5,7 +5,7 @@ from .dunder_all import get_meta_class, inspect_class
 
 class Plugin:
     name = "flake8-stash"
-    version = "0.7.0"
+    version = "0.8.0"
 
     def __init__(self, tree):
         self.tree = tree
